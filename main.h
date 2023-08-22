80003cb38036c49517a79ac67de93188898b6668
azkamalaz <kamalazenag@gmail.com>
2023-08-22 01:41:32 +0000

    main.h update

--- main.h
+++ main.h
@@ -1,19 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
 
 /**
   *struct formats - hold fromat specifiers & functions.
-  *@format_spec: character.
-  *@print_func: function pointer.
+  *@id: character.
+  *@f: function pointer.
 */
 typedef struct formats
 {
-	char format_spec;
-	int (*print_func)(va_list, int);
-} FormatFunction;
+	char id;
+	int (*f)(va_list, int);
+} match;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -24,5 +27,14 @@ int int_func(va_list, int);
 int print_int_func(int, int);
 int digits_func(unsigned long num, unsigned int targ, const char *digits);
 unsigned int len_func(unsigned int, int);
+int unsigned_func(va_list n, int str_length);
+int octal_func(va_list n, int str_length);
+int hex_func(va_list n, int str_length);
+int HEX_func(va_list n, int str_length);
+int check_uns_func(va_list n, int str_length);
+int binary_func(unsigned int num, int str_length);
+int rot13_func(va_list n, int str_length);
+int non_print_str(va_list n, int str_length);
 
+/*team: Abosede Ajoloko, Kamal Azenag*/
 #endif
