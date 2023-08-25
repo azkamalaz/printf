0d7e482effafe836aabc218ffeb4bf95fe973569
azkamalaz <kamalazenag@gmail.com>
2023-08-25 15:20:45 +0000

    print address function update

--- addr_func.c
+++ addr_func.c
@@ -10,9 +10,16 @@ int print_address(va_list n, int str_length)
 {
 	void *address = va_arg(n, void *);
 
-	str_length += _puts("0x");
-	str_length += digits_func2((unsigned long)address, 16, "0123456789abcdef");
+	if (address == NULL)
+	{
+		str_length += _puts("(nil)");
+	}
+	else
+	{
+		str_length += _puts("0x");
 
+		str_length += digits_func2((unsigned long)address, 16, "0123456789abcdef");
+	}
 	return (str_length);
 }
 /*team: Abosede Ajoloko, Kamal Azenag*/
